800ca5b72ef6ea2b1d47ffd9253d2d45c55d5fd6
cblender <christopher.blender@gmail.com>
2020-08-09 20:07:42 -0400

    Bug detected: When players tie, the POT array does not properly retain stored CARD objects.  Safety commit.

--- javascript.js
+++ javascript.js
@@ -42,13 +42,15 @@ class Deck {
 // -- In the event of a tie, the cards remain in the pot and the new index[0] cards are also added to the pot directly.  Then, another round occurs.
 // -- Once the winner is decided, all cards from the pot are added to the END (bottom) of the winner's library.
 // -- At the end of the round, if one player has all 52 cards, the game ends and that player is the winner.
+
     draw(p1, p2) {
         var pot = [];
         var winner = "";
+        var i = 0;
 
         // while(!winner){
-            var card1 = p1.library.splice(0,1);
-            var card2 = p2.library.splice(0,1);
+            var card1 = p1.library.splice(0,1);     // NOTE!!!!!!  ||  FIND OUT HOW TO PASS THE *CONTENTS* OF library[0] to var!!!!!
+            var card2 = p2.library.splice(0,1);     // IMPORTANT!  ||  MAKE SURE REFERENCES IN WHILE LOOP BELOW WORK PROPERLY!!!!!!!
             pot.push(card1);
             pot.push(card2);
             
@@ -58,44 +60,63 @@ class Deck {
             console.log("TEST WHILE ",card2);
             console.log("TEST WHILE ",pot);  
                                               
-            if (card1.score > card2.score) {
-                console.log(p1.name," played a ",card1,".  ",p2.name," played a ",card2,".");
+            if (card1[0].score > card2[0].score) {
+                console.log(p1.name," played a ",card1[0].value," of ",card1[0].suit,".  ",p2.name," played a ",card2[0].value," of ",card2[0].suit,".");
                 winner = p1.name;
-                p1.library.push(pot);
+                // p1.library.push(pot); // IMPLEMENT LOOP TO PUSH EACH CARD BACK INTO A LIBRARY
+                for(i=0;i<pot.length;i++){
+                    p1.library.push(pot[i][0]);
+                }
+
+                pot = [];
 
                 console.log("TEST IF P1 ",p1.library);                        // TEST LOGGER - COMMENT OUT WHEN FINISHED
             }
 
-            else if (card2.score < card1.score) {
-                console.log(p1.name," played a ",card1,".  ",p2.name," played a ",card2,".");
+            else if (card1[0].score < card2[0].score) {
+                console.log(p1.name," played a ",card1[0].value," of ",card1[0].suit,".  ",p2.name," played a ",card2[0].value," of ",card2[0].suit,".");
                 winner = p2.name;
-                p2.library.push(pot);
+                // p2.library.push(pot);
+                for(i=0;i<pot.length;i++){
+                    p2.library.push(pot[i][0]);
+                }
+
+                pot = [];
 
                 console.log("TEST IF P2 ",p2.library);                        // TEST LOGGER - COMMENT OUT WHEN FINISHED
             }
 
-            else if (card1.score === card2.score) {
-                console.log("You both played a ",card1.value,"!  IT'S WAR!!")
+            else if (card1[0].score === card2[0].score) {
+                console.log("You both played a ",card1[0].value,"!  IT'S WAR!!")
                 var war1 = p1.library.splice(0,1);
                 var war2 = p2.library.splice(0,1);
                 pot.push(war1);
                 pot.push(war2);
                 console.log("You each put the top card of your library into the pot, then there is another round.");
-                console.log("TEST IF WAR LOG POT ",pot);                               // TEST LOGGER - COMMENT OUT WHEN FINISHED
+                
+                console.log("TEST IF WAR ",pot);                               // TEST LOGGER - COMMENT OUT WHEN FINISHED
             }
         // } // END WHILE LOOP
 
-        console.log(winner," is the winner!")
-        console.log(p1.name," has ",(p1.library.length-1)," cards.  ",p2.name," has ",(p2.library.length-1)," cards.")
+        if (winner){
+            console.log(winner," is the winner!")
+        }
+        else {
+            console.log("There are ",pot.length," cards in the pot.")
+        }
+        console.log(p1.name," has ",(p1.library.length)," cards.  ",p2.name," has ",(p2.library.length)," cards.")
 
     } // END DRAW FUNCTION
     
 
+// COMMAND: DRAW A RANDOM CARD (***DEPRECIATED***)    
     drawRandom() {
         // This method draws a random card from the deck, but is not used for the game of WAR
         let chosenCard = this.cards.splice([(Math.floor(Math.random()*this.cards.length))], 1)
         return chosenCard
     }
+
+
 
 // COMMAND: SHUFFLE DECK
     shuffle() {
@@ -167,6 +188,8 @@ console.log(playerTwo.name);
 WAR.shuffle();
 
 WAR.split(playerOne, playerTwo);
+
+console.log(playerOne.name," has ",(playerOne.library.length)," cards.  ",playerTwo.name," has ",(playerTwo.library.length)," cards.")
 
 WAR.draw(playerOne, playerTwo);
 
